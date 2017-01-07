34faca4a327586d17c8036553a35c5736d67d0d3
Nicolas Schmitt <nicolas.schmitt@outlook.fr>
2017-01-07 17:01:45 +0100

    import from ios dump

--- import/ios.dump.js
+++ import/ios.dump.js
@@ -11,34 +11,38 @@ let config = new Config().load();
 
 logger.loglevel = config.loglevel;
 
-class Snorlax {
+class IOSDump {
     convert() {
-        return fs.readdirAsync('snorlax')
+        return fs.readdirAsync('ios.dump')
+                .then(files => _.filter(files, f => _.endsWith(f, 'req.raw.bin')))
                 .then(files => {
                     if (files.length == 0) throw new Error('no file to import');
 
-                    let date = files[0].substring(0, files[0].indexOf('.'));
-                    let when = moment(date, 'YYMMDDHHmmSSSS');
+                    let date = _.trimEnd(files[0], '.req.raw.bin');
+                    let when = moment(+date);
                     let folder = when.format('YYYYDDMM.HHmmss');
                     logger.info('Dest folder: data/%s', folder);
+
                     try {
                         fs.mkdirSync('data');
                     } catch(e) {}
                     try {
                         fs.mkdirSync('data/' + folder);
                     } catch(e) {}
-                    return {
-                                folder: folder,
-                                files: files,
-                            };
+                    return fs.writeFileAsync(`data/${folder}/.info`, '(from iOS dump)', 'utf8')
+                            .then(() => {
+                                return {
+                                    folder: folder,
+                                    files: files,
+                                };
+                            });
                 })
                 .then(data => {
                     return Promise.map(data.files, file => {
-                        let timestamp = file.substring(0, file.indexOf('.'));
-                        let when = moment(timestamp, 'YYMMDDHHmmSSSS');
+                        let timestamp = _.trimEnd(file, '.req.raw.bin');
                         return {
                             file: file,
-                            when: when.valueOf(),
+                            when: +timestamp,
                         };
                     })
                     .then(files => {
@@ -50,16 +54,16 @@ class Snorlax {
                 })
                 .then(files => {
                     let reqId = 0;
-                    return Promise.map(files.files, file => this.handleFile(++reqId, files.folder, file));
+                    return Promise.map(files.files, file => this.handleReqFile(++reqId, files.folder, file));
                 })
                 .then(files => {
                     return files.length;
                 });
     }
 
-    handleFile(reqId, folder, file) {
+    handleReqFile(reqId, folder, file) {
         logger.info('Convert file %s in folder %s', file.file, folder);
-        return fs.readFileAsync(`snorlax/${file.file}`)
+        return fs.readFileAsync(`ios.dump/${file.file}`)
                 .then(data => {
                     return {
                         id: reqId,
@@ -70,12 +74,25 @@ class Snorlax {
                 .then(data => {
                     let id = _.padStart(reqId, 5, 0);
                     return fs.writeFileAsync(`data/${folder}/${id}.req.bin`, JSON.stringify(data, null, 4), 'utf8');
-                });
+                })
+                .then(() => this.handleResFile(reqId, folder, file));
+    }
+
+    handleResFile(reqId, folder, file) {
+        let resfile = _.trimEnd(file.file, '.req.raw.bin') + '.res.raw.bin';
+        if (fs.existsSync(`ios.dump/${resfile}`)) {
+            return fs.readFileAsync(`ios.dump/${resfile}`)
+                    .then(data => Buffer.from(data).toString('base64'))
+                    .then(data => {
+                        let id = _.padStart(reqId, 5, 0);
+                        return fs.writeFileAsync(`data/${folder}/${id}.res.bin`, data, 'utf8');
+                    });
+        }
     }
 }
 
-let snorlax = new Snorlax();
-snorlax.convert()
+let iOSDump = new IOSDump();
+iOSDump.convert()
 .then(num => {
     logger.info('%s file(s) converted.', num);
     process.exit();
